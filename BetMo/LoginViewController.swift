e7b266c7a248a30ae822979dd724f6b49bf84a18
Sahil Arora <sahil.ec@gmail.com>
2014-10-08 18:45:23 -0700

    Initial seed for BetMo project.
    This also includes following features implemented:
    1. Facebook login implemented
    2. Linking of an existing Facebook user enabled
    3. Added an empty HomeViewController for SahilAmoli to work with
    4. Created groups for organizing code

---
+++ BetMo/LoginViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  BetMo
+//
+//  Created by Sahil Arora on 10/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+    @IBAction func onLoginButton(sender: AnyObject) {
+        
+        if PFUser.currentUser() == nil {
+            
+            println("Login new user")
+            
+            var permissions: [String] = ["email"]
+            
+            PFFacebookUtils.logInWithPermissions(permissions, {
+                (user: PFUser!, error: NSError!) -> Void in
+                if user == nil {
+                    NSLog("Uh oh. The user cancelled the Facebook login.")
+                } else if user.isNew {
+                    NSLog("User signed up and logged in through Facebook!")
+                    self.performSegueWithIdentifier("loginSegue", sender: self)
+                } else {
+                    NSLog("User logged in through Facebook!")
+                    self.performSegueWithIdentifier("loginSegue", sender: self)
+                }
+            })
+            
+        } else {
+            
+            if !PFFacebookUtils.isLinkedWithUser(PFUser.currentUser()) {
+                println("Attempting to Link current user")
+                PFFacebookUtils.linkUser(PFUser.currentUser(), permissions:nil, {
+                    (succeeded: Bool!, error: NSError!) -> Void in
+                    if succeeded ?? false {
+                        NSLog("Woohoo, user logged in with Facebook!")
+                        println("Linked current user")
+                        self.performSegueWithIdentifier("loginSegue", sender: self)
+                    } else {
+                        println("Error linking current user")
+                    }
+                })
+            } else {
+                println("Current user already linked")
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            }
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
+//---------SAHILARORA---------
+//Reference code that I will use for logout
+//    func onLogout() {
+//        PFFacebookUtils.unlinkUserInBackground(user, {
+//            (succeeded: Bool!, error: NSError!) -> Void in
+//            if succeeded {
+//                NSLog("The user is no longer associated with their Facebook account.")
+//            }
+//        })
+//    }
---
+++ BetMo/tmp.m
@@ -0,0 +1,9 @@
+//
+//  tmp.m
+//  BetMo
+//
+//  Created by Sahil Arora on 10/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
