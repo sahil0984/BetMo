928327a91b1fe6627aa12cdd16e94ec28bd3dbaa
Sahil Arora <sahil.ec@gmail.com>
2014-10-10 23:28:14 -0700

    Added view controller for friends list. Also added search for friends.

---
+++ BetMo/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  BetMo
+//
+//  Created by Sahil Arora on 10/10/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
