52f71c8de7cc2d51e9ce77469366455d2ce9b056
Sahil Amoli <sahilamoli@gmail.com>
2014-10-13 01:32:05 -0700

    Fade in images in the Bets feed

--- BetMo/BetCell.swift
+++ BetMo/BetCell.swift
@@ -133,9 +133,13 @@ class BetCell: UITableViewCell {
             timestampLabel.text = betInfo.getCreatedAt()
 
             var urlRequest = NSURLRequest(URL: NSURL(string: profileImageUrl))
+            self.profileImageView.alpha = 0
             NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse?, data: NSData?, connectionError: NSError?) -> Void in
                 if connectionError == nil && data != nil {
                     self.profileImageView.image = UIImage(data: data!)
+                    UIView.animateWithDuration(0.5, animations: { () -> Void in
+                        self.profileImageView.alpha = 1.0
+                    })
                 }
             }
         }
