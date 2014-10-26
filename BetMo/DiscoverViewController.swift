97e024ac13c3a5ca50a500d9151e6e8a92583882
Sahil Amoli <sahilamoli@gmail.com>
2014-10-25 19:32:15 -0700

    -Added logic to update cards as we remove/accept -Added new default "no bet" card at the bottom

--- BetMo/DiscoverViewController.swift
+++ BetMo/DiscoverViewController.swift
@@ -12,22 +12,29 @@ class DiscoverViewController: UIViewController {
 
     @IBOutlet weak var cardViewOne: CustomCellNib!
     @IBOutlet weak var cardViewTwo: CustomCellNib!
+    @IBOutlet weak var noMoreBetsView: CustomCellNib!
+
+    var superViewCenter: CGFloat!
+
     var activeCardView: CustomCellNib!
     var bets: [Bet] = [Bet]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        superViewCenter = self.view.center.x
+        noMoreBetsView.noContentView = true
+
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         // Do any additional setup after loading the view.
         BetMoClient.sharedInstance.getAllDiscoverableBets({ (bets, error) -> () in
             if error == nil {
                 self.bets = bets!
-                self.updateCards()
                 println(self.bets)
                 MBProgressHUD.hideHUDForView(self.view, animated: true)
-                self.cardViewOne.bet = self.bets[1]
-                self.cardViewTwo.bet = self.bets[0]
+                var bet = self.bets[0] as Bet
+                println(bet.getDescription())
+                self.cardViewOne.bet = self.bets[0]
+                self.cardViewTwo.bet = self.bets[1]
             }
         })
     }
@@ -38,8 +45,7 @@ class DiscoverViewController: UIViewController {
     }
     
     @IBAction func onPan(panGestureRecognizer: UIPanGestureRecognizer) {
-        let cardView = panGestureRecognizer.view!
-        let superViewCenter = self.view.center.x
+        let cardView = panGestureRecognizer.view! as CustomCellNib
 
         let point = panGestureRecognizer.locationInView(view)
         let velocity = panGestureRecognizer.velocityInView(view)
@@ -62,25 +68,41 @@ class DiscoverViewController: UIViewController {
                 UIView.animateWithDuration(0.3, animations: { () -> Void in
                     cardView.center = CGPoint(x: -500, y: cardView.center.y)
                     panGestureRecognizer.setTranslation(CGPointZero, inView: self.view)
+                    }, completion: { (Bool) -> Void in
+                        self.updateCards(cardView)
                 })
             } else if velocityX > 0 && superViewCenter < (currentCenterX - 100) {
                 // If the user ended with a positive velocity and surpassed our position threshold, move the card out of view to the right
                 UIView.animateWithDuration(0.3, animations: { () -> Void in
                     cardView.center = CGPoint(x: 500, y: cardView.center.y)
                     panGestureRecognizer.setTranslation(CGPointZero, inView: self.view)
+                }, completion: { (Bool) -> Void in
+                    self.updateCards(cardView)
                 })
             } else {
                 // The user didn't surpass our threshold of accepting or rejecting, move the card back into it's original position
                 UIView.animateWithDuration(0.1, animations: { () -> Void in
-                    cardView.center = CGPoint(x: superViewCenter, y: cardView.center.y)
+                    cardView.center = CGPoint(x: self.superViewCenter, y: cardView.center.y)
                     panGestureRecognizer.setTranslation(CGPointZero, inView: self.view)
                 })
             }
         }
     }
 
-    func updateCards() {
-        
+    func updateCards(removedCard: CustomCellNib) {
+        bets.removeAtIndex(0)
+        if bets.count < 2 {
+            removedCard.removeFromSuperview()
+        } else if cardViewOne == removedCard {
+            cardViewOne.bet = bets[1]
+            view.bringSubviewToFront(cardViewTwo)
+            cardViewOne.center = CGPoint(x: self.superViewCenter, y: cardViewOne.center.y)
+            self.view.layoutIfNeeded()
+        } else {
+            cardViewTwo.bet = bets[1]
+            view.bringSubviewToFront(cardViewOne)
+            cardViewTwo.center = CGPoint(x: self.superViewCenter, y: cardViewTwo.center.y)
+        }
     }
 
     /*
